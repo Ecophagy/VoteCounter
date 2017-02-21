73326fa4afa93194c098d3d3da669b013f9b44db
killjoy01 <pmarinucci@sbcglobal.net>
2017-02-21 13:22:32 -0800

    Updated Player.cs

--- VoteCounter/src/Player.cs
+++ VoteCounter/src/Player.cs
@@ -8,30 +8,48 @@ namespace VoteCounter
 {
     class Player
     {
-        public int numberOfVotes { get; set; }                                               //number of votes player has
-        public List<string> nicknameList { get; set; }                                       //all names player goes by. 
-        public string mainName { get; set; }                                                 //forum name. 
-        public DateTime lastPost { get; set; }                                               //the last day&time this player posted. 
+        public int numberOfVotes { get; set; }                                            //number of votes player has
+      //  { get { return this.numberOfVotes; } set { this.numberOfVotes = value; } }                                               
+        public List<string> nicknameList { get; set; }                                                    //all names player goes by. 
+            // { get { return this.nicknameList; } set { this.nicknameList = value; } }                                    
+        public string mainName { get; set; }                                                               //forum name. 
+              //  { get {return this.mainName; } set {this.mainName = value; } }                                                
+        public DateTime lastPost { get; set; }                                                             //the last day&time this player posted.
+            //   { get {return this.lastPost; } set {this.lastPost = value; } }                                               
 
-      public  Player()
+        public  Player()
         {
             numberOfVotes = 1;
             mainName = ("Foobar");
             lastPost = new DateTime();
         }
 
-        public Player(string mainName, int numberOfVotes )
+        public Player(Player p)
+            {
+            this.numberOfVotes = p.numberOfVotes;
+            this.mainName = p.mainName;
+            lastPost = p.lastPost;
+            nicknameList = p.nicknameList;
+        }
+
+        public Player(string mainName, int numberOfVotes, List<string> nicknames)
         {
             this.numberOfVotes = numberOfVotes;
             this.mainName = mainName;
             lastPost = new DateTime();
+            nicknameList = nicknames;
         }
 
         //assumes no multivoters. 
-        public Player(string mainName)
+        public Player(string mainName, List<string> nicknames)
         {
-            Player p = new Player(mainName, 1);
-
+            Player p = new Player(mainName, 1, nicknames);
+        }
+        
+        //also assumes no multivoters
+            public Player(string mainName)
+        {
+            Player p = new Player(mainName, 1, new List<string>());
         }
     }
 }
