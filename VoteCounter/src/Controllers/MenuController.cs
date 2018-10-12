c53079a95150e9cefa9f07ef08fdcc9d6e891323
Ecophagy <olly.spoon@gmail.com>
2018-10-12 20:58:27 +0100

    Moved files, added VoteCountController and SaveLoadController

---
+++ VoteCounter/src/Controllers/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web.Script.Serialization;
+
+namespace VoteCounter.src.Controllers
+{
+    class SaveLoadController
+    {
+        public void SaveGame(String fileName, GameState state)
+        {
+            var json = new JavaScriptSerializer().Serialize(state);
+
+            var file = new System.IO.StreamWriter(fileName);
+
+            file.Write(json);
+            file.Close();
+        }
+
+        public GameState LoadGame(string fileName)
+        {
+            var file = new System.IO.StreamReader(fileName);
+
+            var json = file.ReadToEnd();
+            file.Close();
+
+            return new JavaScriptSerializer().Deserialize<GameState>(json);
+        }
+    }
+
+
+}
