560d9f22699023c725cea291b5f2a172e14bec8b
Ecophagy <olly.spoon@gmail.com>
2018-10-12 21:02:20 +0100

    Added url controller

---
+++ VoteCounter/src/Controllers/UrlController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoteCounter.src.Controllers
+{
+    class UrlController
+    {
+        public bool ValidateUrl(string url)
+        {
+            return (url.Contains("mtgsalvation"));
+        }
+    }
+}
